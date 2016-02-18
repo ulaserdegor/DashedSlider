40db905f133814042174ec4cc647e8ef1f2fd1b5
ayalcinkaya <ahmetyk@gmail.com>
2016-02-19 01:56:59 +0200

    pod created

---
+++ Pod/Classes/DashedSlider.swift
@@ -0,0 +1,21 @@
+//
+//  DashedSlider.swift
+//
+//
+//  Created by Ahmet Yalcinkaya on 1/31/16.
+//  Copyright © 2016 ayalcinkaya
+//
+
+import UIKit
+
+class DashedSlider: UISlider {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
